66388ce6099c7ea18f83f5543fce43c7a2ef7189
tookwak5-sudo <tookwak5@gmail.com>
2026-08-25 12:33:20 +0900

    daomybatis 구현

--- final-prj/src/main/java/com/kh/finalprj/dao/AttachDaoMybatis.java
+++ final-prj/src/main/java/com/kh/finalprj/dao/AttachDaoMybatis.java
@@ -1,5 +1,63 @@
 package com.kh.finalprj.dao;
 
-public class AttachDaoMybatis {
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.finalprj.dto.AttachDto;
+
+@Repository
+public class AttachDaoMybatis implements AttachDao{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public int sequence() {
+		return sqlSession.selectOne("mapper.attach.sequence");
+	}
+	@Override
+	public void insert(AttachDto attachDto) {
+		sqlSession.insert("mapper.attach.add", attachDto);
+	}
+	@Override
+	public AttachDto selectOne(int attachNo) {
+		return sqlSession.selectOne("mapper.attach.find", attachNo);
+	}
+	@Override
+	public AttachDto selectOne(Integer attachNo) {
+		if(attachNo == null) return null;
+		return sqlSession.selectOne("mapper.attach.find", attachNo);
+	}
+	@Override
+	public boolean delete(int attachNo) {
+		return sqlSession.delete("mapper.attach.delete", attachNo) > 0;
+	}
+	@Override
+	public List<AttachDto> selectList(List<Integer> attachNumbers) {
+		if(attachNumbers == null || attachNumbers.isEmpty())
+			return List.of();
+		return sqlSession.selectList("mapper.attach.findList", attachNumbers);
+	}
+	@Override
+	public List<AttachDto> selectListByProject(int projectNo) {
+		return sqlSession.selectList("mapper.attach.selectListByProject", projectNo);
+	}
+	@Override
+	public List<AttachDto> selectListByProjectAndKeyword(int projectNo, String keyword) {
+		Map<String, Object> param = Map.of(
+			"projectNo", projectNo,
+			"keyword", keyword
+		);
+		return sqlSession.selectList("mapper.attach.selectListByProjectAndKeyword", param);
+	}
+	
+	
+	
+	
+	
+	
 	
 }
