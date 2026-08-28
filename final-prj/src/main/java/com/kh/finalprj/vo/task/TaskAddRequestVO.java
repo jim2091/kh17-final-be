5ca6605df9c7f4f27e75e0b136c48a7c9aa6edb9
YSH0209 <yoonseunghoon0209@gmail.com>
2026-08-28 16:13:11 +0900

    업무등록 수정 완료

--- final-prj/src/main/java/com/kh/finalprj/vo/task/TaskAddRequestVO.java
+++ final-prj/src/main/java/com/kh/finalprj/vo/task/TaskAddRequestVO.java
@@ -2,32 +2,36 @@ package com.kh.finalprj.vo.task;
 
 import java.sql.Timestamp;
 import java.util.List;
-
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Schema(name = "업무 생성 객체")
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class TaskAddRequestVO {
-	
-	private int taskNo;
-	private int projectNo;
-	private String taskTitle;
-	private String taskContent;
-	private int assignedMemberNo;
-	private String assignedMemberName;
-	private String taskStatus;
-	private Timestamp taskStart;
-	private Timestamp taskEnd;
-	private String taskCategory;
-	private String taskPriority;
-	private Integer taskProgress;
-	private String taskWriterName;
-	private Timestamp taskCtime;
-	private Timestamp taskUtime;
-	
-	private List<Integer> collaboratorMemberNos; // 협업자로 등록할 프로젝트 멤버 번호 목록
+    private int taskNo;
+    private int projectNo;
+    private String taskTitle;
+    private String taskContent;
+
+    private Integer assignedMemberNo; 
+
+    private String assignedMemberName;
+    private String taskStatus;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Timestamp taskStart;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Timestamp taskEnd;
+
+    private String taskCategory;
+    private String taskPriority;
+    private Integer taskProgress;
+    private String taskWriterName;
+    private Timestamp taskCtime;
+    private Timestamp taskUtime;
+    
+    private List<Integer> collaboratorMemberNos;
 }
